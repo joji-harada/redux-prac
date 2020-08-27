6263a47ef7e70155db163291f05c13e08c4e5b1f
George Allison <49288165+joji-harada@users.noreply.github.com>
2020-08-27 15:25:33 -0700

    init

---
+++ reduxtest.js
@@ -0,0 +1,30 @@
+const { createStore } = require('redux');
+
+const initialState = {
+    age:21
+};
+
+const myReducer = (state = initialState, action) => {
+    const newState = {...state};
+
+    if(action.type === 'ADD'){
+        newState.age += 1;
+    }
+    if(action.type === 'SUBTRACT'){
+        newState.age -= 1;
+    }
+
+    return newState;
+}
+
+const store = createStore(myReducer);
+
+store.subscribe(() => {
+    console.log('state changed:\t', store.getState());
+})
+
+
+
+store.dispatch({type:'ADD'});
+
+store.dispatch({type:'SUBTRACT'});
